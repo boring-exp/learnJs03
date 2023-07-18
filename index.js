75024b0e30ea5970a741cc9773e2e683fdf3b756
JinweiOS <jinwei.peng@beingthink.com>
2023-07-18 14:41:58 +0800

    first committ

---
+++ index.js
@@ -0,0 +1,14 @@
+// 1. 打印
+console.log('hello world')
+
+// 2. 全局对象
+// console.log(globalThis)
+
+// 3. 注释
+/**
+ * 多行注释
+ * 1234
+ */
+
+// console.log(/* 传入等待打印的参数 */'hello note')
+console.log(this)
